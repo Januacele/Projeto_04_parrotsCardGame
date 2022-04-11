bd80c3a3a6ce3784d08eda5ffeb4830be5a669c1
Januacele <januacelevieira@hotmail.com>
2022-04-10 23:09:14 -0300

    Embaralhando

--- script.js
+++ script.js
@@ -2,42 +2,81 @@
 let cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
+// Função para virar cartas 
 function flipCard() {
+  if (lockBoard) return;
+  
+
   this.classList.add('flip');
 
-
+// Checando se as cartas viradas são iguais ou diferentes
 if (!hasFlippedCard) {
   hasFlippedCard = true;
   firstCard = this;
   return;
   }
+
   secondCard = this;
-  hasFlippedCard = false;
+  
 
   checkForMatch();
 }
 
+//Função que checa se as cartas clicadas são iguais 
 function checkForMatch() {
-  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+  if (firstCard.dataset.framework === secondCard.dataset.framework){
     disableCards();
     return;
-  }
-  unflipCards();
+    }
+
+    unflipCards();
+
 }
 
-function disableCards() {
+function disableCards(){
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
+//Desvirando as cartas diferentes 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
-  firstCard.classList.remove('flip');
-  secondCard.classList.remove('flip');
-  }, 1500);
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    
+    resetBoard();
+  }, 1300);
   }
+
+
+  function resetBoard() {
+
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  (function shuffle() {
+       cards.forEach(card => {
+         let ramdomPos = Math.floor(Math.random() * 12);
+         card.style.order = ramdomPos;
+       });
+     })();
+
+
+    //let EndGame = prompt(String("Deseja jogar novamente? [sim/não]"));
+   // if (EndGame === 'sim'){
+    //  resetBoard();
+   // } else {
+    //  alert("Obrigado!")
+   // }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
